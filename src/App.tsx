badf645999624861294ff4bb76a04e2cb35aad52
simon njoroge <mukirisimon22@gmail.com>
2024-06-21 17:13:01 +0300

    todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,57 @@
-
-import './App.scss'
-import Header from './components/header'
-import Footer from './components/footer'
+import './App.scss';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
 
   const senddata = () => {
-    {
-      const contains: any = document.querySelector(".container")
-      const dataadd: any = document.querySelector(".adding")
-      const div = document.createElement("div")
-      div.classList.add("Dta")
-      const data = document.createElement("div")
-      div.classList.add("DATA")
-      const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        const det =document.createElement("div")
-        det.classList.add("dele")
-      const deleted = document.createElement("button")
-      deleted.innerHTML="delete"
-      const par = dataadd.value
-      det.append(deleted)
-      data.append(checkbox)
-      data.append(par)
-      div.append(data)
-      div.append(det)
-      contains.append(div)
-    }
+    const contains: any = document.querySelector(".container");
+    const dataadd: any = document.querySelector(".adding");
 
-  }
+    const div = document.createElement("div");
+    div.classList.add("Dta");
+    const dataDiv = document.createElement("div");
+    dataDiv.classList.add("DATA");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox"
+    const span = document.createElement("span");
+    span.textContent = dataadd.value;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+
+  
+    deleteButton.addEventListener("click", () => {
+      contains.removeChild(div); 
+    });
+
+    dataDiv.appendChild(checkbox);
+    dataDiv.appendChild(span);
+    div.appendChild(dataDiv);
+    div.appendChild(deleteButton);
+    contains.appendChild(div);
+  };
+
   return (
     <>
-      
-        <div className="whole">
-          <Header />
-          <div className="add">
-            <input type="checkbox" />
-            <input type="text" className='adding' />
-            <div className="sub">
-              <button onClick={senddata}>Submit</button>
-            </div>
+      <div className="whole">
+        <Header />
+        <div className="add">
+          <input type="checkbox" />
+          <input type="text" className='adding' />
+          <div className="sub">
+            <button onClick={senddata}>Submit</button>
           </div>
-          <div className='entire'>
+        </div>
+        <div className='entire'>
           <div className="container">
-
+            
           </div>
           <Footer />
         </div>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
